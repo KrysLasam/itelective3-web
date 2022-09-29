674892c4acb70d6217700e2fd72f762f4a591a5f
Krys <67639516+KrysLasam@users.noreply.github.com>
2022-09-29 08:26:32 +0800

    New Update

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const weather = require('weather-js');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    weather.find({ search: 'Davao, PH', degreeType: 'C' }, function (err, result) {
+        var weather_data = null;
+        if (err) console.log(err);
+        else {
+            weather_data = result;
+        }
+        res.render('index', { panahon: weather_data });
+    });
+
+})
+app.get('/other', function (req, res) {
+    res.render('other');
+})
+
+app.listen(3000);
